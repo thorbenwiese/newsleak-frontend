069620e42d01945a10178951aa1c1e7b223ad0ad
alvinrindra <alvinrindra@gmail.com>
2017-05-10 17:59:02 +0200

    Add feature figures: doc white/blacklisting and keyword graph

--- Projektplan/ProjM_WILPS17_Wiese_Fazrie/js/reveal.js
+++ Projektplan/ProjM_WILPS17_Wiese_Fazrie/js/reveal.js
@@ -56,7 +56,7 @@
 			progress: true,
 
 			// Display the page number of the current slide
-			slideNumber: false,
+			slideNumber: true,
 
 			// Push each slide change to the browser history
 			history: false,
@@ -1426,7 +1426,7 @@
 	 * target element.
 	 *
 	 * remaining height = [ configured parent height ] - [ current parent height ]
-	 * 
+	 *
 	 * @param {HTMLElement} element
 	 * @param {number} [height]
 	 */
@@ -3900,7 +3900,7 @@
 			// If there are media elements with data-autoplay,
 			// automatically set the autoSlide duration to the
 			// length of that media. Not applicable if the slide
-			// is divided up into fragments. 
+			// is divided up into fragments.
 			// playbackRate is accounted for in the duration.
 			if( currentSlide.querySelectorAll( '.fragment' ).length === 0 ) {
 				toArray( currentSlide.querySelectorAll( 'video, audio' ) ).forEach( function( el ) {
